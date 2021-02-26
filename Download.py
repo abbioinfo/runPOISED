473d02eb0a44e968914e8294d2dccec9a341a8b2
abhinav kaushik <55936153+abbioinfo@users.noreply.github.com>
2021-02-26 15:28:00 -0800

    Add files via upload

--- Download.py
+++ Download.py
@@ -1,36 +1,12 @@
 import gdown
 from zipfile import ZipFile
 
-url = 'https://drive.google.com/uc?id=1kbs5z1DLpo6o_LqZlrwcwtK6XSGYKtXI'
-output = 'POISED_live.zip'
+url = 'https://drive.google.com/uc?id=1OsnbZ9Or18PngdRKqQQ42WWEkZ5same9'
+output = 'POISED.zip'
 gdown.download(url, output, quiet=False)
 
 # Create a ZipFile Object and load sample.zip in it
-with ZipFile('POISED_live.zip', 'r') as zipObj:
+with ZipFile('POISED.zip', 'r') as zipObj:
    # Extract all the contents of zip file in current directory
    zipObj.extractall()
-
-
-
-
-url = 'https://drive.google.com/uc?id=1_JpvX4slqiHU0kDXbd4tO6RtFaCWrXQS'
-output = 'POISED_label.zip'
-gdown.download(url, output, quiet=False)
-
-# Create a ZipFile Object and load sample.zip in it
-with ZipFile('POISED_label.zip', 'r') as zipObj:
-   # Extract all the contents of zip file in current directory
-   zipObj.extractall()
-
-
-
-url = 'https://drive.google.com/uc?id=1EK2IPc2U6NKyotZJDU2493SD3cIAgKtw'
-output = 'POISED_handgated.zip'
-gdown.download(url, output, quiet=False)
-
-# Create a ZipFile Object and load sample.zip in it
-with ZipFile('POISED_handgated.zip', 'r') as zipObj:
-   # Extract all the contents of zip file in current directory
-   zipObj.extractall()
-
 
